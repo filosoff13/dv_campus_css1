41d4702fbf799f6395a3f552177f6b7c69d210f2
Vitalii Vitkovskyi <filosoff13@gmail.com>
2019-11-07 23:11:32 +0200

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,33 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        less: {
-            dev: {
-                options: {
-                    compress: false,
-                    yuicompress: false,
-                    optimization: 2,
-                    strictImports: true,
-                    sourceMap: true,
-                    sourceMapFilename: 'pub/css/responsive-styles.css.map', // where file is generated and located
-                    sourceMapURL: 'responsive-styles.css.map', // the complete url and filename put in the compiled css file
-                    sourceMapBasepath: 'pub', // Sets sourcemap base path, defaults to current working directory.
-                    sourceMapRootpath: '/', // adds this path onto the sourcemap filename and less file paths
-                },
-                files: {
-                    "pub/css/responsive-styles.css": "assets/dist/less/responsive-styles.less"
-                }
+        cssmin: {
+            options: {
+                sourceMap: true,
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
             },
-            prod: {
-                options: {
-                    compress: false,
-                    yuicompress: false,
-                    optimization: 2,
-                    strictImports: true
-                },
+            target: {
                 files: {
-                    "assets/dist/css/responsive-styles.css": "assets/dist/less/responsive-styles.less"
+                    'pub/css/styles.css': [
+                        'assets/dist/css/responsive-styles-processed.css'
+                    ]
                 }
             }
         },
 
         postcss: {
-            dev: {
-                options: {
-                    map: true,
-                    processors: [
-                        require('autoprefixer')({
-                            overrideBrowserslist: ['last 2 versions',  'ie 11']
-                        })
-                    ]
-                },
-                src: 'pub/css/responsive-styles.css',
-                dest: 'pub/css/responsive-styles.min.css'
+            options: {
+                processors: [
+                    require('autoprefixer')({
+                        overrideBrowserslist: ['last 2 versions',  'ie 11']
+                    })
+                ]
             },
-
-            prod: {
-                options: {
-                    processors: [
-                        require('autoprefixer')({
-                            overrideBrowserslist: ['last 2 versions',  'ie 11']
-                        }),
-                        require('cssnano')()
-                    ]
-                },
+            dist: {
                 src: 'assets/dist/css/responsive-styles.css',
-                dest: 'pub/css/responsive-styles.min.css'
+                dest: 'assets/dist/css/responsive-styles-processed.css'
             }
         },
-
         imagemin: {
             dynamic: {
                 files: [{
@@ -67,31 +37,19 @@ module.exports = function(grunt) {
                     dest: 'pub/images/'
                 }]
             }
-        },
-
-        watch: {
-            less: {
-                files: ['assets/dist/less/**/*.less'],
-                tasks: ['less:dev', 'postcss:dev']
-            },
-            css: {
-                files: ['pub/css/*.min.css'],
-                options: {
-                    livereload: true,
-                }
-            },
-            imagemin: {
-                files: 'assets/dist/images/**/*.{png,jpg,gif}',
-                tasks: ['imagemin']
-            }
         }
+
     });
 
-    grunt.loadNpmTasks('grunt-contrib-less');
+
+    // grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['less:prod', 'postcss:prod', 'imagemin']);
-    grunt.registerTask('dev', ['less:dev', 'postcss:dev', 'imagemin', 'watch']);
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['postcss', 'cssmin', 'imagemin']);
+    // grunt.registerTask('dev', ['less:dev', 'postcss:dev', 'imagemin', 'watch']);
 };
